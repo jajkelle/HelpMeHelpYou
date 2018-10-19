7b65d64275d376504c5ebaab58e44974fe69cafe
rjzhao1 <44123617+rjzhao1@users.noreply.github.com>
2018-10-19 01:40:07 -0700

    Update db.py

--- models/db.py
+++ models/db.py
@@ -5,7 +5,7 @@
 # Auth is for authenticaiton and access control
 # -------------------------------------------------------------------------
 from gluon.contrib.appconfig import AppConfig
-from gluon.tools import Auth
+from gluon.tools import Aut
 
 # -------------------------------------------------------------------------
 # This scaffolding model makes your app work on Google App Engine too
