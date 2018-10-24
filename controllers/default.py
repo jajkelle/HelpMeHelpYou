0a010b089dd80d657e154db49cd0326ae8f8a4e6
Srija Konjarla <32750996+srijakonjarla@users.noreply.github.com>
2018-10-24 15:19:09 -0700

    Update default.py

--- controllers/default.py
+++ controllers/default.py
@@ -6,8 +6,7 @@
 
 # ---- example index page ----
 def index():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict(message=T('Welcome to HelpMeHelpYou!'))
 
 # ---- API (example) -----
 @auth.requires_login()
