0ede56c22b459cf357709e7fd88b99a93c2e0e05
jajkelle <jajkelle@ucsc.edu>
2018-10-25 13:50:01 -0700

    added message features to database

---
+++ models/db_chats.py
@@ -0,0 +1,23 @@
+import random
+import time; now=time.time()
+import datetime;
+timestamp=datetime.datetime.today()
+today=datetime.date.today()
+
+db.define_table('chat_line',
+    Field('username'),
+    Field('description','text',default=''),
+    Field('created_on','datetime',default=timestamp),
+    Field('created_by',db.auth_user))
+
+db.chat_line.username.requires=IS_NOT_EMPTY()
+db.chat_line.access_types=['none','read','read/chat']
+db.chat_line.public_fields=['username','description']
+
+db.define_table('message_table',
+    Field('chat_line',db.chat_line),
+    Field('body','text',default=''),
+    Field('posted_on','datetime',default=timestamp),
+    Field('posted_by',db.auth_user))
+
+db.message_table.body.requires=IS_NOT_EMPTY()
