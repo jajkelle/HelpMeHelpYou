708b47afb4d9a275e9aa584656d758cc4cf04db4
donaldjhui <44250286+donaldjhui@users.noreply.github.com>
2018-10-28 21:34:54 -0700

    modified menu screen, add and view tabs work

--- menu.py
+++ menu.py
@@ -13,8 +13,35 @@ response.menu = [
 # provide shortcuts for development. you can remove everything below in production
 # ----------------------------------------------------------------------------------------------------------------------
 
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# ----------------------------------------------------------------------------------------------------------------------
+# this is the main application menu add/remove items as required
+# ----------------------------------------------------------------------------------------------------------------------
+
+response.menu = [
+    (T('Home'), False, URL('admin', 'default', 'design/%s' % request.application))
+]
+
+# ----------------------------------------------------------------------------------------------------------------------
+# provide shortcuts for development. you can remove everything below in production
+# ----------------------------------------------------------------------------------------------------------------------
+
 if not configuration.get('app.production'):
     _app = request.application
     response.menu += [
-        (T('Index'), False, URL('HelpMeHelpYou', 'default', 'index')),
+        (T('Resources'), False, '#', [
+            (T('View'), False, URL('welcome', 'default', 'view_resource')),
+            (T('Add'), False,
+             URL(
+                 'welcome', 'default', 'add_resource')),
+            (T('Delete'), False,
+             URL(
+                 'welcome', 'default', 'edit/%s/models/db.py' % _app)),
+            (T('Edit'), False,
+             URL(
+                 'welcome', 'default', 'edit/%s/models/db.py' % _app)),
+        ]),
+        (T('Index'), False, URL('welcome', 'default', 'index')),
     ]
