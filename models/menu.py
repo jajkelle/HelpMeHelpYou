293e240b49bfe70e5c5b33adb1290cec1223a195
rjzhao1 <44123617+rjzhao1@users.noreply.github.com>
2018-11-28 23:52:48 -0800

    Merge Menu to master

--- models/menu.py
+++ models/menu.py
@@ -6,23 +6,29 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 response.menu = [
-                 (T('Home'), False, URL('default', 'index'), [])
-                 ]
+    (T('Home'), False, URL('default', 'index'), [])
+]
+if session.auth:
+    user_id = session.auth.user.id
+else:
+    user_id = None
 
-if not configuration.get('app.production'):
-    _app = request.application
-    response.menu += [
-                      (T('Resources'), False, '#', [
-                                                    (T('View'), False, URL('HelpMeHelpYou', 'default', 'view_resource')),
-                                                    (T('Add'), False,
-                                                     URL(
-                                                         'HelpMeHelpYou', 'default', 'add_resource')),
-                                                    (T('Delete'), False,
-                                                     URL(
-                                                         'HelpMeHelpYou', 'default', 'delete_resource')),
-                                                    (T('Edit'), False,
-                                                     URL(
-                                                         'HelpMeHelpYou', 'default', 'edit/%s/models/db.py' % _app)),
-                                                    ]),
-                      (T('Index'), False, URL('HelpMeHelpYou', 'default', 'index')),
-                      ]
+response.menu += [
+    (T('Resources'), False,'#', [
+      (T('My Resource'), False,URL('list_resources',args = user_id) if session.auth else URL('default','index')),
+      (T('List Users'), False, URL('list_id')),
+      ((T('Add Resources'), False, URL('add_resources') if session.auth else URL('default','index'))),
+      (T('Delete Resources'), False, URL('delete_resource',args = user_id) if session.auth else URL('default','index')),
+      (T('Edit Resources'), False, URL('edit_resource',args = user_id)),
+      (T('Search'), False,URL('default', 'search_resource'))
+        ]
+    )
+]
+
+response.menu += [
+        ((T('Category'),False, '#', [
+                (T(row.Name), False, URL('list_resource_by_category',args=row.Name), []) for row in db(db.category).select()]))
+]
+# ----------------------------------------------------------------------------------------------------------------------
+# provide shortcuts for development. you can remove everything below in production
+# ----------------------------------------------------------------------------------------------------------------------
