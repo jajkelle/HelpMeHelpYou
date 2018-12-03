3d6a3d0b049b78ff902bffee7d57e60553ebf99b
rjzhao1 <44123617+rjzhao1@users.noreply.github.com>
2018-12-02 22:23:34 -0800

    fixed bugs and removed unnecessary functions

--- controllers/default.py
+++ controllers/default.py
@@ -8,37 +8,52 @@
 #https://helpmehelpyou/deafult/list_user_resources/category/
 
 # ---- example index page ----
+#Homepage of our website
 def index():
     return dict(message=T('Welcome to HelpYouHelpMe'))
 
-
+#takes in user_id as argument
+#list the resource of the user with that user id
 def list_resources():
     user_id = request.args(0,cast=int)
     row=db(db.resources.resource_owner==user_id).select()
     return locals()
 
+#takes in user_id as argument
+#Goes through that users resource database and delete the requested resource
 def delete_resource():
     user_id = request.args(0,cast=int)
     db(db.resources.resources_id == request.vars.resources_id).delete()
-    specifications = db(db.resources.resource_owner==user_id).select()
+    row = db(db.resources.resource_owner==user_id).select()
     return locals()
 
+#list all the user id in our database
 def list_id():
     row = db(db.auth_user).select()
     return locals()
 
+#takes in category as argument
+#returns a list of resources that belongs in that category
 def list_resource_by_category():
     category_name = request.args(0)
     category=db.category(Name=category_name)
     row = db(db.resources.resources_category==category).select()
     return locals()
 
+#takes in user_id of the user in session
+#Return a SQL form for the user to add resources to
+#Enter the new resources into data base and redirects user to their own resource page
+#If not in session, redirect to homepage
 def add_resources():
     user_id = session.auth.user.id
     db.resources.resource_owner.default = user_id
-    form = SQLFORM(db.resources).process(next='list_user_resources/[resource_owner]')
+    form = SQLFORM(db.resources).process(next=URL('list_resources',args = user_id))
     return locals()
 
+#takes in user_id of the user in session
+#Return a form for user to enter the resource they want to edit
+#Enter the information of the resource is now changed in our database
+#If not in session, redirect to homepage
 def edit_resource():
     user_id = request.args(0,cast=int)
     edit_id = request.vars.resources_id
@@ -55,17 +70,12 @@ def edit_resource():
     specifications = db(db.resources.resource_owner==user_id).select()
     return locals()
 
+#Adds a profile page
 def profile():
     return dict(form=auth.profile())
 
-def category():
-    row = db(db.category).select()
-    return locals()
-
-def test():
-    form = SQLFORM(db.resources)
-    return locals()
-
+#User input in a SQLForm to find the resources they are looking for
+#Returns a list of all the resources that relates to what the user is looking for
 def search_resource():
     form = SQLFORM.factory(Field('title', requires=IS_NOT_EMPTY()))
     if form.accepts(request):
@@ -74,7 +84,14 @@ def search_resource():
         people = db(query).select()
     else:
         people= []
-    return dict(form=form,results=people)
+    return dict(form=form,result=people)
+
+#takes a resource id as input
+#return the information of that resource
+def list_single_resource():
+    resource_id = request.args(0,cast=int)
+    row=db(db.resources.resources_id==resource_id).select()
+    return locals()
 
 # ---- API (example) -----
 @auth.requires_login()
